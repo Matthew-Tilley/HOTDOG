ade9cad7835925898e16693da0ba2b77d19a3f6e
Matt Tilley <mtilley255@gmail.com>
2018-07-04 08:10:00 -0500

    Added Firebase config script to JS file.

---
+++ hotdog.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyABK0eNB_wdNzCavXUjxqeLTb_j_G2VSnc",
+    authDomain: "hotdog-9177a.firebaseapp.com",
+    databaseURL: "https://hotdog-9177a.firebaseio.com",
+    projectId: "hotdog-9177a",
+    storageBucket: "",
+    messagingSenderId: "1086619698425"
+  };
+  firebase.initializeApp(config);
